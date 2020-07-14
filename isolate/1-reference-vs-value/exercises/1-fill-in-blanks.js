69b9203e77e5b50c98937707fbae67028321993d
Miroslav Veljanoski <veljanoski.miroslav@gmail.com>
2020-07-14 12:37:22 +0200

    Complete reverence vs value exercises

--- isolate/1-reference-vs-value/exercises/1-fill-in-blanks.js
+++ isolate/1-reference-vs-value/exercises/1-fill-in-blanks.js
@@ -2,8 +2,8 @@
 
 // psst.  use JS Tutor & the debugger!
 
-const array1 = _;
-const array2 = _;
+const array1 = [true];
+const array2 = array1;
 
 const test1 = array1 === array2;
 console.assert(test1, 'Test 1');
@@ -11,4 +11,3 @@ console.assert(test1, 'Test 1');
 const test2a = array1[0] === true;
 const test2b = array2[0] === true;
 console.assert(test2a && test2b, 'Test 2');
-
--- isolate/1-reference-vs-value/exercises/2-fill-in-blanks.js
+++ isolate/1-reference-vs-value/exercises/2-fill-in-blanks.js
@@ -2,8 +2,8 @@
 
 // psst.  use JS Tutor & the debugger!
 
-const array1 = _;
-const array2 = _;
+const array1 = [false];
+const array2 = [false];
 
 const test1 = array1 !== array2;
 console.assert(test1, 'Test 1');
@@ -11,4 +11,3 @@ console.assert(test1, 'Test 1');
 const test2a = array1[0] === false;
 const test2b = array2[0] === false;
 console.assert(test2a && test2b, 'Test 2');
-
